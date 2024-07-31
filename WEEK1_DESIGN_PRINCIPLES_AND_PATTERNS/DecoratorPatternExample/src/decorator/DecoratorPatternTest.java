59e38930bf819de6d5229a88af6005c4cd1a7845
Subhrankita Pradhan <pradhanrimi3486@gmail.com>
2024-07-31 09:40:05 +0530

    Add files via upload

---
+++ WEEK1_DESIGN_PRINCIPLES_AND_PATTERNS/DecoratorPatternExample/src/decorator/DecoratorPatternTest.java
@@ -0,0 +1,12 @@
+package decorator;
+
+public class DecoratorPatternTest {
+    public static void main(String[] args) {
+        Notifier emailNotifier = new EmailNotifier();
+        Notifier smsNotifier = new SMSNotifierDecorator(emailNotifier);
+        Notifier slackNotifier = new SlackNotifierDecorator(smsNotifier);
+
+        System.out.println("Sending notification through Email, SMS, and Slack:");
+        slackNotifier.send("Hello, World!");
+    }
+}
---
+++ WEEK1_DESIGN_PRINCIPLES_AND_PATTERNS/DecoratorPatternExample/src/decorator/NotifierDecorator.java
@@ -0,0 +1,13 @@
+package decorator;
+
+public abstract class NotifierDecorator implements Notifier {
+    protected Notifier wrappedNotifier;
+
+    public NotifierDecorator(Notifier notifier) {
+        this.wrappedNotifier = notifier;
+    }
+
+    public void send(String message) {
+        wrappedNotifier.send(message);
+    }
+}
