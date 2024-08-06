f0a9e8225e17a66853a860401f20dcae1bbd1879
Subhrankita Pradhan <pradhanrimi3486@gmail.com>
2024-08-06 12:59:07 +0530

    Add files via upload

---
+++ WEEK2_SpringCore _Maven/Spring_Boot_Application/LibraryManagement/src/main/java/com/library/model/Book.java
@@ -0,0 +1,32 @@
+package com.library.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
